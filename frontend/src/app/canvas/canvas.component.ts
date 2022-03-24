a68cf9604f35fa59a1fd984132312ed6f455556c
Asylbek Kydraliev <asyl.kydraliev@gmail.com>
2022-03-24 13:57:33 +0600

    Draw circle on canvas

--- frontend/src/app/canvas/canvas.component.ts
+++ frontend/src/app/canvas/canvas.component.ts
@@ -1,15 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
+
+interface Message {
+  username: string,
+  text: string,
+}
+interface ServerMessage {
+  type: string,
+  message: Message
+}
 
 @Component({
   selector: 'app-canvas',
   templateUrl: './canvas.component.html',
   styleUrls: ['./canvas.component.sass']
 })
-export class CanvasComponent implements OnInit {
 
-  constructor() { }
+export class CanvasComponent implements AfterViewInit, OnDestroy {
+  ws!: WebSocket;
+  @ViewChild('canvas') canvas!: ElementRef;
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.ws = new WebSocket('ws://localhost:8000/canvas');
+    this.ws.onclose = () => console.log('ws closed');
+
+    this.ws.onmessage = event => {
+      const decodedMessage: ServerMessage = JSON.parse(event.data);
+
+      console.log(decodedMessage);
+    }
+  }
+
+  onClickCanvas(event: MouseEvent){
+    const x = event.offsetX;
+    const y = event.offsetY;
+
+    const canvasElement: HTMLCanvasElement = this.canvas.nativeElement;
+    const ctx = canvasElement.getContext("2d")!;
+    ctx.beginPath();
+    ctx.arc(x,y,10,0,2*Math.PI);
+    ctx.stroke();
+  }
+
+  ngOnDestroy(){
+    this.ws.close();
   }
 
 }
